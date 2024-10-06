73448284ab0cd692a7616da5d80821317f3a04ef
douglasnou <dodonou3@gmail.com>
2024-10-06 10:34:57 -0300

    corrigindo o middleware do cors

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ import express from "express";
 import publicRoutes from "./routes/public.js";
 import privateRoutes from "./routes/private.js";
 import auth from "./middlewares/auth.js";
-import corsMiddlware from "./middlewares/cors.js";
+import cors from "cors";
 
 export const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.use('/', corsMiddlware, publicRoutes);
-app.use('/', corsMiddlware, auth, privateRoutes);
+app.use('/', publicRoutes);
+app.use('/', auth, privateRoutes);
 
 app.listen(5000, ()=>{
     console.log(`Server is running.`);

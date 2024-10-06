d013d3504ffae6ab33ba2bdcea148501e9b043d9
douglasnou <dodonou3@gmail.com>
2024-10-06 11:46:05 -0300

    correções

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ import cors from "cors";
 
 export const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 app.use('/', publicRoutes);
 app.use('/', auth, privateRoutes);
